f09a39d87214b54e1f7e7ca5306801d7401ce30b
Angelo Huaraca <ahuaracab@gmail.com>
2023-03-11 00:58:10 -0500

    print int, long, double, boolean and String

--- src/main/java/com/ob/Main.java
+++ src/main/java/com/ob/Main.java
@@ -2,6 +2,16 @@ package com.ob;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 1;
+        long y = 2454;
+        double z = 5.5;
+        boolean isRunning =  false;
+        String cadena = "Hola Mundo";
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(isRunning);
+        System.out.println(cadena);
     }
 }
